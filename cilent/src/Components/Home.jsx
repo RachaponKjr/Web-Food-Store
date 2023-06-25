8c11f642b727a6708f8d12d23cc117bb6da0e417
RachaponKjr <rachapon.navy@gmail.com>
2023-06-25 10:08:49 +0700

    6/25/23

--- cilent/src/Components/Home.jsx
+++ cilent/src/Components/Home.jsx
@@ -7,7 +7,7 @@ import { useAppContext } from "../Contexts/appContext";
 const Home = () => {
   const [dataFood,setdataFood] = useState([])
 
-  const { favorites ,addToFavorites,removeToFavorites} = useAppContext()
+  const { favorites ,addToFavorites,removeToFavorites,favItem} = useAppContext()
 
   const arrFavorites = Object.values(favorites)
   console.log(arrFavorites)
@@ -69,41 +69,14 @@ const Home = () => {
         {/* ITEM LIST */}
 
           {/* LIKE LIST */}
-          <div className=" bg-slate-300 w-full h-max my-4">
+          <div className=" w-full h-max my-4">
           <h1 className="text-xl flex items-center gap-1 text-mocha">
           รายการที่ชอบ
           <AiFillHeart className="top-[-0.2rem] relative text-[#ff5a00]" />
         </h1>
-        <div className=" py-1 flex justify-between ">  
-          {/* {listfav.map((item,index)=>{
-            return(
-<div className="w-[12rem] h-max overflow-hidden  bg-gray-400 relative drop-shadow-md " key={index}>
-            {item.favorite === true ? (
-              <AiFillHeart
-              onClick={() => {handleFavorite(item.id);}}
-                className="absolute right-2 top-2 text-2xl cursor-pointer text-red-500 duration-500"
-                
-              />
-            ) : (
-              <AiOutlineHeart
-              onClick={() => {handleFavorite(item.id);}}
-                className="absolute right-2 top-2 text-2xl cursor-pointer text-mocha hover:text-red-600 duration-500"
-              />
-            )}
-            <img
-              src={item.imgurl}
-              alt="img_Food"
-              className="object-cover object-center w-full h-[8rem]"
-            />
-            <div className="bg-white h-auto flex justify-center flex-col items-center p-2 gap-1">
-            <h2 className=" text-xl text-mocha">{item.name}</h2>
-            
-              <h4 className=" text-lg text-green-700">{item.price} ฿</h4>
-              <button className="bg-whip text-mocha hover:bg-cream hover:text-white duration-300 h-10 w-28 top-4 my-2">Add To Cart</button>
-            </div>
-          </div>
-            )
-          })} */}
+        <div className=" py-1 flex justify-start gap-5 flex-wrap ">  
+          
+          {favItem()}
           
         </div>
           </div>
--- cilent/src/Contexts/appContext.jsx
+++ cilent/src/Contexts/appContext.jsx
@@ -28,9 +28,32 @@ const AppContextProvider = ({children}) => {
         setFavorites(newItem)
     }
 
+    const favItem = () =>{
+        if(favorites.length > 0){
+            return favorites.map((item,index)=>{
+                return(
+                <div className="w-[12rem] h-max overflow-hidden  bg-gray-400 relative drop-shadow-md " key={index}>    
+                <img
+                  src={item.imgurl}
+                  alt="img_Food"
+                  className="object-cover object-center w-full h-[8rem]"
+                />
+                <div className="bg-white h-auto flex justify-center flex-col items-center p-2 gap-1">
+                <h2 className=" text-xl text-mocha">{item.name}</h2>
+                
+                  <h4 className=" text-lg text-green-700">{item.price} ฿</h4>
+                  <button className="bg-whip text-mocha hover:bg-cream hover:text-white duration-300 h-10 w-28 top-4 my-2">Add To Cart</button>
+                </div>
+              </div>)
+            })
+        }else{
+            return <h1 className=" w-full h-full flex items-center  justify-center">คุณไม่มีรายการโปรด</h1>
+        }
+    }
+
     return(
         <AppContext.Provider
-        value={{favorites,addToFavorites,removeToFavorites}}>
+        value={{favorites,addToFavorites,removeToFavorites,favItem}}>
             {children}
         </AppContext.Provider>
     )
